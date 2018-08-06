64b47ca619626d4044280ebf82899b85530e1829
pirjotuulia <pirjoleppanen@nic.fi>
2018-08-06 19:13:25 +0300

    ekat filet

---
+++ src/main/java/fi/academy/ravintolapeli/RavintolapeliApplication.java
@@ -0,0 +1,12 @@
+package fi.academy.ravintolapeli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RavintolapeliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RavintolapeliApplication.class, args);
+    }
+}
---
+++ src/test/java/fi/academy/ravintolapeli/RavintolapeliApplicationTests.java
@@ -0,0 +1,16 @@
+package fi.academy.ravintolapeli;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RavintolapeliApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
