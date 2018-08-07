4a8bd76e68063a4e7b6109df17d046c4aad03c45
mikkokallio <mikko.m.kallio@gmail.com>
2018-08-07 17:03:35 +0300

    more front end functionality; cards now load data from db

--- src/main/java/fi/academy/ravintolapeli/controllers/MissionController.java
+++ src/main/java/fi/academy/ravintolapeli/controllers/MissionController.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+@CrossOrigin
 @RestController
 public class MissionController {//rest-rajapinta pelimissioihin
     private MissionRepository repo;
