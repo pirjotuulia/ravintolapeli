acd0c518ea8ae5c3443256fbcfc1cac0c829d9f3
pirjotuulia <pirjoleppanen@nic.fi>
2018-08-08 10:22:15 +0300

    GameController, GameStats uusi versio, kommentteja

--- src/main/java/fi/academy/ravintolapeli/objects/GameStats.java
+++ src/main/java/fi/academy/ravintolapeli/objects/GameStats.java
@@ -1,10 +1,11 @@
 package fi.academy.ravintolapeli.objects;
 
 import fi.academy.ravintolapeli.objects.restaurant.Restaurant;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
-
+@Component
 public class GameStats {//pelitilanneolio
     private int health;
     private double money;
@@ -15,6 +16,13 @@ public class GameStats {//pelitilanneolio
     private List<LastMove> moves;
 
     public GameStats() {
+        this.health = 100;
+        this.money = 100;
+        this.foodcriticName = "Hugh Fearnley-Whittingstall";
+        this.hand = new ArrayList<>();
+        this.playedMissions = new ArrayList<>();
+        this.missionMode = false;
+        this.moves = new ArrayList<>();
     }
 
     public GameStats(int health, int money, String foodcriticName, List<Mission> hand, List<Mission> playedMissions) {
@@ -29,23 +37,36 @@ public class GameStats {//pelitilanneolio
 
     @Override
     public String toString() {
-        return "{" +
-                "health: " + health +
-                ", money: " + money +
-                ", foodcriticName: '" + foodcriticName + '\'' +
-                ", hand: " + hand +
-                ", playedMissions: " + playedMissions +
+        return "GameStats{" +
+                "health=" + health +
+                ", money=" + money +
+                ", foodcriticName='" + foodcriticName + '\'' +
+                ", hand=" + hand +
+                ", playedMissions=" + playedMissions +
+                ", missionMode=" + missionMode +
+                ", moves=" + moves +
                 '}';
     }
 
-    public void makeMove(int index, Restaurant restaurant, double distance, int time) {
-        playCard(index);
-        int restaurantScore = restaurant.getGrades().get(0).getScore();
-        LastMove move = new LastMove(restaurantScore, restaurant.getLongitude(), restaurant.getLatitude(), time, distance, false);
-        useHealth(restaurantScore);
-        useMoney(move.getUsedMoney());
-        this.moves.add(move);
-        setMissionMode(false);
+    //vuoron päätteeksi tehdään tämä setti tietojen päivittämiseksi ja vuoron lisäämiseksi
+    public GameStats makeMove(int index, Restaurant restaurant, double distance, int time) {
+        playCard(index);// siirtää pelatun kortin kädestä pelattuihin
+        int restaurantScore = restaurant.getGrades().get(0).getScore(); // tallettaa ravintolan scoren
+        LastMove move = new LastMove(restaurantScore, restaurant.getLongitude(), restaurant.getLatitude(), time, distance, false); //tekee pelatun vuoron tiedoista LastMove-olion
+        useHealth(restaurantScore);// vähentää terveyttä ravintolan scoren verran
+        useMoney(move.getUsedMoney()); //käyttää rahaa matkan taksan verran
+        this.moves.add(move); // lisää pelatun vuoron vuorojen listalle
+        setMissionMode(false); // asettaa missionModen kortinvalinta-tilaan
+        return this;//palauttaa GameStats-olion (tarvitaanko tätä edes?)
+    }
+
+    public void gameOver() {//kun peli on pelattu loppuun
+        this.setHand(new ArrayList<>()); //tyhjennetään käsi, riippumatta siitä, oliko se jo tyhjä vai loppuiko peli muusta syystä
+        this.setPlayedMissions(new ArrayList<>()); //tyhjennetään pelatut kortit
+        this.setMissionMode(false); //asetetaan missionMode kortinvalinta-tilaan
+        //LastMoves jätetään ennalleen, eli pelihistoria säilyy
+        this.setHealth(100);//jos uusi vuoro alkaa täydellä terveydellä
+        this.setMoney(100);//jos uusi vuoro alkaa täysillä rahoilla
     }
 
     public int useHealth(int used) {
@@ -111,4 +132,6 @@ public class GameStats {//pelitilanneolio
     public void setPlayedMissions(List<Mission> playedMissions) {
         this.playedMissions = playedMissions;
     }
+
+
 }
